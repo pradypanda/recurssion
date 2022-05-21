4129243b9ae6b963a55baa6365191f2f35500f46
prady.panda@gmail.com <prady.panda@gmail.com>
2022-05-21 15:14:40 +0530

    Initial Commit

---
+++ dsa/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String args[])
+    {
+        m1();
+
+    }
+
+    static void m1()
+    {
+        System.out.println("Heloo");
+        m2();
+    }
+    static void m2()
+    {
+        System.out.println("Heloo");
+        m3();
+    }
+
+    static void m3()
+    {
+        System.out.println("Heloo");
+        m4();
+    }
+
+    static void m4()
+    {
+        System.out.println("Hello");
+    }
+
+}
